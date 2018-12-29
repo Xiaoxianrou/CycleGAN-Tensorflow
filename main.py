66b8daf9bb250252e87f87c59eb44d0319e6565d
Xiaoxianrou <wjydd666@gmail.com>
2018-12-29 12:14:23 +0000

    'init'

--- main.py
+++ main.py
@@ -1,10 +1,65 @@
 import tensorflow as tf
 import numpy as np
+import argparse
 import os
 
 from PIL import Image
+from models import CycleGAN
 
-from .models import *
+DATASET_PATH = './datasets/vangogh2pokemon'
+
+parser = argparse.ArgumentParser(description="Run commands")
+parser.add_argument('--cycle_loss_lambda', type=float, default=10, help='Cycle Consistency Loss coefficient')
+parser.add_argument('--instance_normalization', default=True, type=bool, help="Use instance norm instead of batch norm")
+parser.add_argument('--batch_size', default=1, type=int, help="Batch size")
+parser.add_argument('--image_size', default=128, type=int, help="Image size")
+parser.add_argument('--epoch_num', default=200, type=int, help="Epoch number")
+args = parser.parse_args()
+print(args)
 
 
+def read_image(path):
+    try:
+        img = Image.open(path)
+        img = img.resize((args.image_size, args.image_size))
+        img_m = np.array(img)
+    except IOError:
+        print('fail to load image!')
 
+    if len(img_m.shape) != 3 or img_m.shape[2] != 3:
+        print('Wrong image {} with shape {}'.format(path, img_m.shape))
+        return None
+
+    img_m = img_m.astype(np.float32) / 255.0
+    return img_m
+
+
+def read_images(base_dir):
+    image_dataset = {}
+    for dir_name in ['trainA', 'trainB', 'testA', 'testB']:
+        images_dir = os.path.join(base_dir, dir_name)
+        images = []
+        for idx, file in enumerate(os.listdir(images_dir)):
+            file_path = os.path.join(images_dir, file)
+            image = read_image(file_path)
+            if image is not None:
+                images.append(image)
+        image_dataset[dir_name] = images
+    return image_dataset
+
+
+def main():
+    image_dataset = read_images(DATASET_PATH)
+    trainA = image_dataset['trainA']
+    trainB = image_dataset['trainB']
+
+
+    summary_writer = tf.summary.FileWriter('./logs')
+    gan = CycleGAN(args)
+
+    with tf.Session() as sess:
+        gan.train(sess, summary_writer, trainA, trainB)
+
+
+if __name__ == "__main__":
+    main()
--- models.py
+++ models.py
@@ -1,13 +1,21 @@
 import tensorflow as tf
 import numpy as np
-import os
+import random
 
-from .layers import *
+from PIL import Image
+from layers import *
+
+REAL_LABEL = 1
+FAKE_LABEL = 0
+
+INITIAL_ITER = 100
+DECAY_ITER = 100
+INITIAL_LR = 0.0002
 
 
 class Generator(object):
     def __init__(self, name, is_train, norm='instance', activation='relu', image_size=128):
-        print('Init Generator %s', name)
+        print('Init Generator', name)
         self.name = name
         self._train = is_train
         self._norm = norm
@@ -33,7 +41,7 @@ class Generator(object):
 
 class Discriminator(object):
     def __init__(self, name, is_train, norm='instance', activation='leaky'):
-        print('Init Discriminator %s', name)
+        print('Init Discriminator', name)
         self.name = name
         self._is_train = is_train
         self._norm = norm
@@ -54,12 +62,163 @@ class Discriminator(object):
             return D
 
 
-class CycleGAN():
-    def __init__(self):
-        pass
+class HistoryQueue(object):
+    def __init__(self, shape=[128, 128, 3], size=50):
+        self._size = size
+        self._shape = shape
+        self._count = 0
+        self._queue = []
 
-    def train(self):
-        pass
+    def query(self, image):
+        if len(image.shape) == 3:
+            image = np.expand_dims(image, axis=0)
+        if self._size == 0:
+            return image
+        if self._count < self._size:
+            self._count += 1
+            self._queue.append(image)
+            return image
 
-    def test(self):
-        pass
+        p = random.random()
+        if p > 0.5:
+            idx = random.randrange(0, self._size)
+            ret = self._queue[idx]
+            self._queue[idx] = image
+            return ret
+        else:
+            return image
+
+
+class CycleGAN(object):
+    def __init__(self, args):
+        self._epoch_num = args.epoch_num
+        self._batch_size = args.batch_size
+        self._image_size = args.image_size
+        self._cycle_loss_lambda = args.cycle_loss_lambda
+
+        self.image_shape = [self._image_size, self._image_size, 3]
+        self.is_train = tf.placeholder(tf.bool, name='is_train')
+        self.lr = tf.placeholder(tf.float32, name='learning_rate')
+
+        self.image_a = tf.placeholder(tf.float32, [self._batch_size] + self.image_shape, name='image_a')
+        self.image_b = tf.placeholder(tf.float32, [self._batch_size] + self.image_shape, name='image_b')
+        self.history_fake_a = tf.placeholder(tf.float32, [None] + self.image_shape, name='history_fake_a')
+        self.history_fake_b = tf.placeholder(tf.float32, [None] + self.image_shape, name='history_fake_b')
+
+        self.build()
+        self.summary_op = tf.summary.merge_all()
+
+    def build(self):
+        # build Generator
+        G_ab = Generator('G_ab', is_train=self.is_train, norm='instance', activation='relu',
+                         image_size=self._image_size)
+        G_ba = Generator('G_ba', is_train=self.is_train, norm='instance', activation='relu',
+                         image_size=self._image_size)
+
+        # build Discriminator
+        D_a = Discriminator('D_a', is_train=self.is_train, norm='instance', activation='leaky')
+        D_b = Discriminator('D_b', is_train=self.is_train, norm='instance', activation='leaky')
+
+        # Generate images (a->b->a and b->a->b)
+        self.image_ab = G_ab(self.image_a)
+        self.image_aba = G_ba(self.image_ab)
+        self.image_ba = G_ba(self.image_b)
+        self.image_bab = G_ab(self.image_ba)
+
+        # Discriminate real/fake images
+        D_real_a = D_a(self.image_a)
+        D_fake_a = D_a(self.image_ba)
+        D_real_b = D_b(self.image_b)
+        D_fake_b = D_b(self.image_ab)
+        D_history_fake_a = D_a(self.history_fake_a)
+        D_history_fake_b = D_b(self.history_fake_b)
+
+        # Least squre loss for GAN discriminator
+        self.loss_D_a = tf.contrib.gan.losses.wargs.least_squares_discriminator_loss(D_real_a, D_history_fake_a,
+                                                                                     real_label=REAL_LABEL,
+                                                                                     fake_label=FAKE_LABEL)
+        self.loss_D_b = tf.contrib.gan.losses.wargs.least_squares_discriminator_loss(D_real_b, D_history_fake_b,
+                                                                                     real_label=REAL_LABEL,
+                                                                                     fake_label=FAKE_LABEL)
+        # Least squre loss for GAN generator
+        loss_G_ab_ls = tf.contrib.gan.losses.wargs.least_squares_generator_loss(D_fake_b, real_label=REAL_LABEL)
+        loss_G_ba_ls = tf.contrib.gan.losses.wargs.least_squares_generator_loss(D_fake_a, real_label=REAL_LABEL)
+
+        # L1 norm for cycle loss
+        loss_cycle_aba = tf.reduce_mean(tf.abs(self.image_a - self.image_aba))
+        loss_cycle_bab = tf.reduce_mean(tf.abs(self.image_b - self.image_bab))
+        self.loss_cycle = self._cycle_loss_lambda * (loss_cycle_aba + loss_cycle_bab)
+
+        # GAN generator loss
+        self.loss_G_ab = loss_G_ab_ls + self.loss_cycle
+        self.loss_G_ba = loss_G_ba_ls + self.loss_cycle
+
+        # Optimizer
+        self.optimizer_D_a = tf.train.AdamOptimizer(learning_rate=self.lr, beta1=0.5) \
+            .minimize(self.loss_D_a, var_list=D_a.var_list)
+        self.optimizer_D_b = tf.train.AdamOptimizer(learning_rate=self.lr, beta1=0.5) \
+            .minimize(self.loss_D_b, var_list=D_b.var_list)
+        self.optimizer_G_ab = tf.train.AdamOptimizer(learning_rate=self.lr, beta1=0.5) \
+            .minimize(self.loss_G_ab, var_list=G_ab.var_list)
+        self.optimizer_G_ba = tf.train.AdamOptimizer(learning_rate=self.lr, beta1=0.5) \
+            .minimize(self.loss_G_ba, var_list=G_ba.var_list)
+
+    def train(self, sess, summary_writer, data_A, data_B):
+        print('Start training')
+        data_size = min(len(data_A), len(data_B))
+        batch_num = data_size // self._batch_size
+        lr_decay = INITIAL_LR / DECAY_ITER
+
+        history_a = HistoryQueue(shape=self.image_shape, size=50)
+        history_b = HistoryQueue(shape=self.image_shape, size=50)
+
+        init_all = tf.initializers.global_variables()
+        sess.run(init_all)
+
+        # Training epoch loop
+        for epoch in range(self._epoch_num):
+            print("In the epoch ", epoch)
+            # shuffle datasets
+            random.shuffle(data_A)
+            random.shuffle(data_B)
+            batch_idxs = min(len(data_A), len(data_B)) // self._batch_size
+
+            # Training batch loop
+            for idx in range(0, batch_idxs):
+                # initial input
+                image_a = np.stack(data_A[idx * self._batch_size:(idx + 1) * self._batch_size])
+                image_b = np.stack(data_B[idx * self._batch_size:(idx + 1) * self._batch_size])
+                fake_a, fake_b = sess.run([self.image_ba, self.image_ab], feed_dict={self.image_a: image_a,
+                                                                                     self.image_b: image_b,
+                                                                                     self.is_train: True})
+                fake_a = history_a.query(fake_a)
+                fake_b = history_b.query(fake_b)
+
+                # fetch result
+                if epoch > INITIAL_ITER:
+                    learning_rate = max(0.0, INITIAL_LR - (epoch - INITIAL_ITER) * lr_decay)
+                else:
+                    learning_rate = INITIAL_LR
+
+                fetches = [self.loss_D_a, self.loss_D_b, self.loss_G_ab, self.loss_G_ba, self.loss_cycle,
+                           self.optimizer_D_a, self.optimizer_D_b, self.optimizer_G_ab, self.optimizer_G_ba]
+                fetched = sess.run(fetches, feed_dict={self.image_a: image_a,
+                                                       self.image_b: image_b,
+                                                       self.is_train: True,
+                                                       self.lr: learning_rate,
+                                                       self.history_fake_a: fake_a,
+                                                       self.history_fake_b: fake_b})
+            if epoch // 50 != 0:
+                self.test(sess, data_A, data_B)
+
+    def test(self, sess, data_A, data_B):
+        for idx in range(3):
+            fetches = [self.image_ba, self.image_bab]
+            image_b = np.expand_dims(data_B[idx], axis=0)
+            image_ba, image_bab = sess.run(fetches, feed_dict={self.image_b: image_b, self.is_train: False})
+            # images = np.concatenate((image_b, image_ba, image_bab), axis=2)
+            # images = np.squeeze(images, axis=0)
+            image_ba = np.squeeze(image_ba, axis=0)
+            img_ba = Image.fromarray((image_ba*255.0).astype('uint8'))
+            img_ba.save()
+            # print(images)
